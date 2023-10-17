473a29fdaea1e512e8de24a2b1c6f17ab6eabbb3
meghnaaxx <meghna.chawla134@gmail.com>
2023-10-17 12:38:13 +0530

    first

---
+++ script.js
@@ -0,0 +1,48 @@
+let currentPage = 1;
+
+function move() {
+    var elem = document.getElementById("myBar");
+    var width = 0;
+    var id = setInterval(frame, 10);
+
+    function frame() {
+        if (width >= 100) {
+            clearInterval(id);
+        } else {
+            width++;
+            elem.style.width = width + '%';
+            elem.innerHTML = width + '%';
+        }
+    }
+}
+function showPage(pageNumber) {
+    const pages = document.querySelectorAll('.page');
+    pages.forEach(page => page.classList.remove('active'));
+    pages[pageNumber - 1].classList.add('active');
+}
+
+function nextPage() {
+    if (currentPage < 15) {
+        currentPage++;
+        move();
+        showPage(currentPage);
+    }
+}
+
+function prevPage() {
+    if (currentPage > 1) {
+        currentPage--;
+        showPage(currentPage);
+    }
+}
+
+document.getElementById('form').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+});
+
+showPage(currentPage);
+
+
+
+
